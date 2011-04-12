09f4e2b3ec5c86eac7d71a936c3a83393acdfadf
Ben Hu <benzheren@gmail.com>
2011-04-11 23:48:13 -0700

    start to use a Box class

--- dropbox/packing.py
+++ dropbox/packing.py
@@ -3,36 +3,44 @@ from string import atoi
 
 from packer import CygonRectanglePacker
 
+class Box(object):
+    """docstring for Box"""
+    def __init__(self, x, y):
+        self.s = min(x, y)
+        self.l = max(x, y)
+    
+    def __cmp__(self, other):
+        return (self.l - other.l) and self.l - other.l or self.s - other.s 
+        
 def read_input(file):
     n = atoi(file.readline())
     def read_boxes(s):
         box = map(atoi, s.split())
-        return (max(box[0], box[1]), min(box[0], box[1]))
+        return Box(box[0], box[1])
     return map(lambda x: read_boxes(file.readline()), xrange(n))
 
 def first_feasible_solution(boxes):
     '''here boxes are sorted boxes'''
-    width = sum(box[1] for box in boxes)
-    height = boxes[0][0]
+    width = sum(box.s for box in boxes)
+    height = boxes[0].l
     print 'area width %d' % width
     packer = CygonRectanglePacker(width, height)
     for box in boxes:
-        print '%d %d' % (box[0], box[1])
+        print '%d %d' % (box.s, box.l)
         print 'try placement 1'
-        placement1 = packer.TryPack(box[1], box[0])
+        placement1 = packer.TryPack(box.s, box.l)
         print 'try placement 2'
-        placement2 = packer.TryPack(box[0], box[1])
-        (placement2 and (placement1.x + box[1]) > (placement2.x + box[0])) and\
-                packer.Pack(box[0], box[1]) or packer.Pack(box[1], box[0])
+        placement2 = packer.TryPack(box.l, box.s)
+        (placement2 and (placement1.x + box.s) > (placement2.x + box.l)) and\
+                packer.Pack(box.l, box.s) or packer.Pack(box.s, box.l)
 
     return packer
 
 def solve(boxes):
-    sorted_boxes = sorted(boxes, key=lambda box: max(box[0], box[1]),
-                          reverse=True)
-    for box in sorted_boxes:
-        print '%d %d' % (box[0], box[1])
-    packer = first_feasible_solution(sorted_boxes)
+    boxes.sort(reverse=True)
+    for box in boxes:
+        print '%d %d' % (box.s, box.l)
+    packer = first_feasible_solution(boxes)
     for i in packer.heightSlices:
         print '%d %d' % (i.x, i.y)
 
